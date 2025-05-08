6c23c577668f1585239d59382643a2049e3ac1fa
Ubuntu <ubuntu@ip-172-31-32-51.eu-west-1.compute.internal>
2025-05-08 14:24:55 +0000

    Add gridsearch stage

--- src/models/grid_search_model.py
+++ src/models/grid_search_model.py
@@ -3,18 +3,26 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 import joblib
 import os
+import yaml
 
-X_train = pd.read_csv("/home/ubuntu/masterDVCexam/examen-dvc/data/preprocessed/X_train_scaled.csv")
-y_train = pd.read_csv("/home/ubuntu/masterDVCexam/examen-dvc/data/preprocessed/y_train.csv").values.ravel()
+# Load params from YAML
+with open("params.yaml") as f:
+    params_yaml = yaml.safe_load(f)
 
 params = {
-    "n_estimators": [50, 100, 200],
-    "max_depth": [5, 10, None],
-    "min_samples_split": [2, 5]
+    "n_estimators": params_yaml["train"]["n_estimators"],
+    "max_depth": params_yaml["train"]["max_depth"],
+    "min_samples_split": params_yaml["train"]["min_samples_split"],
 }
+
+
+X_train = pd.read_csv("data/normalized/X_train_scaled.csv")
+y_train = pd.read_csv("data/split/y_train.csv").values.ravel()
+
+
 
 grid = GridSearchCV(RandomForestRegressor(random_state=42), params, cv=5, scoring="r2", n_jobs=-1)
 grid.fit(X_train, y_train)
 
-os.makedirs("/home/ubuntu/masterDVCexam/examen-dvc/models", exist_ok=True)
-joblib.dump(grid.best_params_, "/home/ubuntu/masterDVCexam/examen-dvc/models/best_params.pkl")
+os.makedirs("models", exist_ok=True)
+joblib.dump(grid.best_params_, "models/best_params.pkl")
